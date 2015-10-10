fd15323afc2de8fc6c481e759758eacafaa11735
Frank Geusch <frank.geusch@kaufda.de>
2015-10-10 23:45:02 +0200

    base parsing done

--- groovy/src/mummy-ctl.groovy
+++ groovy/src/mummy-ctl.groovy
@@ -1,16 +1,12 @@
 #!/usr/bin/env groovy
-import groovy.transform.EqualsAndHashCode
-import groovy.transform.Sortable
-import groovy.transform.ToString
-
 @Grapes([
     @Grab(group='org.xerial', module='sqlite-jdbc', version='3.8.11.2'),
     @Grab(group='org.xerial.thirdparty', module='nestedvm', version='1.0'),
-    @GrabConfig(systemClassLoader=true)
+    @GrabExclude('xml-apis:xml-apis'),
+    @GrabConfig(systemClassLoader=true, initContextClassLoader=true)
 ])
 
-import java.sql.*
-import org.sqlite.SQLite
+import groovy.transform.*
 import groovy.sql.Sql
 
 /**
@@ -19,40 +15,47 @@ import groovy.sql.Sql
  * Created by fudge on 10.10.15.
  */
 
-
+// parse command line
 def cli = new CliBuilder()
-
 cli.with {
     usage = 'mummy-ctl'
     f(longOpt: 'file', 'input file', args: 1, type: String, required: true)
 }
-
-
 def opt = cli.parse(args)
 if (!opt) return -1
 
-File infile = new File(opt.f)
-println "using database ${infile.absolutePath}"
 
-
-def sql = Sql.newInstance("jdbc:sqlite:${infile.absolutePath}", "org.sqlite.JDBC")
-
+// read database
 def qry = """
 select ZHTMLTEXT from ZARTICLECONTENT, ZARTICLE
 where ZARTICLECONTENT.ZARTICLE = ZARTICLE.Z_PK and
 ZARTICLE.ZFLAGGED = 1
 """
 
-sql.eachRow(qry) {
-    println it
-    // parse html, extract author, title, urls
+List<Album> albums = []
+Sql.newInstance("jdbc:sqlite:${new File(opt.f).absolutePath}", "org.sqlite.JDBC").eachRow(qry) { r ->
+    // TODO urls
+    albums << new Album(
+        artist: xtract(r, /Artist:/),
+        title: xtract(r, /Album:/),
+        style: xtract(r, /Style:/)
+    )
+    println r
+    // (DONWLOAD LINKS .. FLAC) each s/href="([^"]*)"/\1/
 }
 
+// ...
+
+// ----------- lib and helper -----------
+
+String xtract(from, regex) {
+    from.toString().split(/\n/).find{ it =~ regex }.replaceAll(/<[^>]*>/, '').replace(regex, '').trim()
+}
 
 @ToString
 @EqualsAndHashCode(excludes = ['urls'])
 @Sortable(excludes = ['urls'])
 class Album {
-    String title, artist
+    String title, artist, style
     List<URL> urls = []
 }
